981a4f55e8a1f5958abfbd837890d5d94cc216b1
mhmdariefh <117131938+mhmdariefh@users.noreply.github.com>
2022-11-06 14:44:30 +0700

    Update Pembeli.java

--- Pembeli.java
+++ Pembeli.java
@@ -1,28 +1,44 @@
 public class Pembeli {
     private String nama;
     private int uang;
-    
+
     public Pembeli() {
 
     }
 
+    /**
+     * @param nama adalah nama dari pembeli
+     * @param uang adalah uang dari pembeli
+     */
     public Pembeli(String nama, int uang) {
         this.nama = nama;
         this.uang = uang;
     }
 
+    /**
+     * @return nama untuk mengembalikan nama
+     */
     public String getNama() {
         return nama;
     }
 
+    /**
+     * @return uang untuk mengembalikan uang
+     */
     public int getUang() {
         return uang;
     }
 
+    /**
+     * @param nama untuk mengubah nilai dari nama
+     */
     public void setNama(String nama) {
         this.nama = nama;
     }
 
+    /**
+     * @param uang untuk mengubah nilai dari uang
+     */
     public void setUang(int uang) {
         this.uang = uang;
     }
